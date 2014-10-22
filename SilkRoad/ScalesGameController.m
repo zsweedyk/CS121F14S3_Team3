135729a3aec9ed680d19afe98bd5d87626727946
seratri <sarahtrisorus@gmail.com>
2014-10-22 08:36:31 -0700

    Added Scales game files

---
+++ SilkRoad/ScalesGameController.m
@@ -0,0 +1,37 @@
+//
+//  ScalesGameController.m
+//  SilkRoad
+//
+//  Created by Sarah Trisorus on 10/20/14.
+//  Copyright (c) 2014 Kate Finlay, Melissa Galonsky, Rachel Macfarlane, and Sarah Trisorus. All rights reserved.
+//
+
+#import "ScalesGameController.h"
+
+@interface ScalesGameController ()
+
+@end
+
+@implementation ScalesGameController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
