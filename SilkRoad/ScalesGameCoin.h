a67f40abdd2bfdc9074be2fb5f7774011625566b
seratri <sarahtrisorus@gmail.com>
2014-10-22 19:10:06 -0700

    Added Coin class for the scales/currency minigame

---
+++ SilkRoad/ScalesGameCoin.h
@@ -0,0 +1,16 @@
+//
+//  ScalesGameCoin.h
+//  SilkRoad
+//
+//  Created by Sarah Trisorus on 10/22/14.
+//  Copyright (c) 2014 Kate Finlay, Melissa Galonsky, Rachel Macfarlane, and Sarah Trisorus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScalesGameCoin : NSObject
+
+@property (assign, nonatomic) NSString* name;
+@property (assign, nonatomic) int weight;
+
+@end
---
+++ SilkRoad/ScalesGameCoin.m
@@ -0,0 +1,28 @@
+//
+//  ScalesGameCoin.m
+//  SilkRoad
+//
+//  Created by Sarah Trisorus on 10/22/14.
+//  Copyright (c) 2014 Kate Finlay, Melissa Galonsky, Rachel Macfarlane, and Sarah Trisorus. All rights reserved.
+//
+
+#import "ScalesGameCoin.h"
+
+@implementation ScalesGameCoin
+
+- (id)init {
+  self = [super init];
+  
+  if (self) {
+    [self setName:@"Coin 0"];
+    [self setWeight:1];
+  }
+  
+  return self;
+}
+
+- (NSString *)description {
+  return [NSString stringWithFormat: @"%@", [self name]];
+}
+
+@end
