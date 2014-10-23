af84b1e4e3d756010e70dfc76e816047c61421fb
Kate Finlay <kate.n.finlay@gmail.com>
2014-10-22 23:33:20 -0700

    Characters now appear in interior views

---
+++ SilkRoad/Character.h
@@ -0,0 +1,13 @@
+//
+//  Character.h
+//  SilkRoad
+//
+//  Created by Katharine Finlay on 10/22/14.
+//  Copyright (c) 2014 Kate Finlay, Melissa Galonsky, Rachel Macfarlane, and Sarah Trisorus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Character : NSObject
+
+@end
---
+++ SilkRoad/Character.m
@@ -0,0 +1,13 @@
+//
+//  Character.m
+//  SilkRoad
+//
+//  Created by Katharine Finlay on 10/22/14.
+//  Copyright (c) 2014 Kate Finlay, Melissa Galonsky, Rachel Macfarlane, and Sarah Trisorus. All rights reserved.
+//
+
+#import "Character.h"
+
+@implementation Character
+
+@end
